a630f769a9d531b018118a17685cff65a54b21e4
EG0 <alex1234567639.fg@gmail.com>
2020-08-19 17:02:54 +0800

    [initial] Programs - tabs

--- src/components/Programs.vue
+++ src/components/Programs.vue
@@ -3,17 +3,37 @@
         <h2>&nbsp;&nbsp;節目資訊</h2>
         <hr>
         <!-- Tab links -->
-        <div class="tab">
-            <button class="tablinks" onclick="openCity(event, 'London')" id="defaultOpen">全部節目</button>
-            <button class="tablinks" onclick="openCity(event, 'Paris')">近期上演</button>
-            <button class="tablinks right" onclick="openCity(event, 'Tokyo')">最新開賣</button>
+        <div class="tabs">
+            <div class="tab" @click="toggleTab('ProgramsAll')"><a>全部節目</a></div>
+            <div class="tab" @click="toggleTab('ProgramsComingSoon')"><a>近期上演</a></div>
+            <div class="tab" @click="toggleTab('ProgramsNewSell')"><a>最新開賣</a></div>
         </div>
+        <!-- Tab content -->
+        <prince :is="currentTab" keep-alive></prince>
   </div>
 </template>
 
 <script>
-export default {
+import ProgramsAll from './ProgramsAll.vue';
+import ProgramsComingSoon from './ProgramsComingSoon.vue';
+import ProgramsNewSell from './ProgramsNewSell.vue';
 
+export default {
+    data () {
+        return {
+            currentTab: 'ProgramsAll' 
+        };
+    },
+    components: {
+        ProgramsAll,
+        ProgramsComingSoon,
+        ProgramsNewSell
+    },
+    methods: {
+        toggleTab: function(tab) {
+            this.currentTab = tab;
+        }
+    }
 }
 </script>
 
@@ -31,5 +51,25 @@ export default {
 }
 .programs > hr{
     border-top: 1px dashed gray;
+}
+.tabs{
+    /* border: 1px solid red; */
+}
+.tab{
+    border: 1px solid;
+    border: 2px solid lightgray;
+    display: inline-block;
+    width: 120px;
+    padding: 10px;
+    text-align: center;
+    font-size: 18px;
+    transition-duration: .3s;
+}
+.tab:hover{
+    cursor: pointer;
+    background-color: lightgray;
+}
+.active{
+    background-color: lightgray;
 }
 </style>
