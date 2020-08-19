8e272880a9a437d1c3ab636bc66d59e0cbc607a7
EG0 <alex1234567639.fg@gmail.com>
2020-08-19 12:49:49 +0800

    [initial] footer

--- src/App.vue
+++ src/App.vue
@@ -2,7 +2,7 @@
   <div id="app">
     <nav class="navbar">
       <div class="nabar-homepage">
-        <router-link :to="'/'"><img src="./assets/img/logo.png"></router-link>
+        <router-link :to="'/'"><img src="./assets/img/iconsert-3.png"></router-link>
       </div>
       <div class="navbar-lists">
         <ul class="navbar-listsUl">
@@ -28,6 +28,42 @@
       </div>
     </nav>
     <router-view/>
+    <!-- advertisement -->
+    <main class="application">
+      <div class="app">
+        <img src="./assets/img/iconcert_app3.png" class="iconcert_app">
+        <p class="tittle ml3">iconcert 手機板上線囉!</p>
+        <p>豐富的資訊、貼心的服務，</p>
+        <p>最完整的演唱會體驗盡在 iconcert!</p>
+        <a href="https://www.apple.com/tw/ios/app-store/" target="_blank" class="ios"><img src="./assets/img/ios_download.png" class="ios_download"></a>
+        <a href="https://play.google.com/store?hl=zh_TW" target="_blank" class="android"><img src="./assets/img/android_download.png" class="android_download"></a>
+      </div>
+    </main>
+    <!-- footer -->
+    <footer>
+      <div class="foot">
+        <div class="about">
+          <a href="#">關於我們</a> <a href="#">隱私權政策</a>
+        </div>
+        <div class="app_icons">
+          <a href="#"><img src="./assets/img/fb.png" class="fb"></a>
+          <a href="#"><img src="./assets/img/twitter.png" class="twitter"></a>
+          <a href="#"><img src="./assets/img/ig.png" class="ig"></a>
+        </div>
+        <div class="iconcert_company">
+          <p>iconcert股份有限公司</p>
+          <p>聯絡電話：03-2345-6789</p>
+          <p>客服專線：0800-092-001</p>
+          <p>聯絡地址：桃園市中大路300號</p>
+        </div>
+
+        <div class="logobox">
+          <img src="./assets/img/iconcert.png" class="lll">
+          <br>
+          <p>Copyright &copy; 2020 iconcert.<br>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;All right reserved.</p>
+        </div>
+      </div>
+    </footer>
   </div>
 </template>
 
@@ -40,4 +76,6 @@ export default {
 <style>
 @import "./assets/css/reset.css";
 @import "./assets/css/navbar.css";
+@import "./assets/css/advertisement.css";
+@import "./assets/css/footer.css";
 </style>
