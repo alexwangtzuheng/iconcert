4c544fb1bbcaa36cf0c007cffe4e026c3dbd1d63
EG0 <alex1234567639.fg@gmail.com>
2020-08-20 10:33:05 +0800

    [initial] Programs - tabs'  content

--- src/components/Programs.vue
+++ src/components/Programs.vue
@@ -1,16 +1,18 @@
 <template>
-  <div class="programs">
-        <h2>&nbsp;&nbsp;節目資訊</h2>
-        <hr>
-        <!-- Tab links -->
-        <div class="tabs">
-            <div class="tab" @click="toggleTab('ProgramsAll')"><a>全部節目</a></div>
-            <div class="tab" @click="toggleTab('ProgramsComingSoon')"><a>近期上演</a></div>
-            <div class="tab" @click="toggleTab('ProgramsNewSell')"><a>最新開賣</a></div>
+initial    <div class="programs-bg">
+        <div class="programs">
+            <h2>&nbsp;&nbsp;節目資訊</h2>
+            <hr>
+            <!-- Tab links -->
+            <div class="tabs">
+                <div class="tab" :class="{ programsAllTabfocus: isProgramsAllTabfocus }" @click="toggleTab('ProgramsAll'),clickProgramsAll()"><a>全部節目</a></div>
+                <div class="tab" :class="{ programsComingSoonTabfocus: isProgramsComingSoonTabfocus }" @click="toggleTab('ProgramsComingSoon'),clickProgramsComingSoon()"><a>近期上演</a></div>
+                <div class="tab" :class="{ programsNewSellTabfocus: isProgramsNewSellTabfocus}" @click="toggleTab('ProgramsNewSell'),clickProgramsNewSell()"><a>最新開賣</a></div>
+            </div>
+            <!-- Tab content -->
+            <prince :is="currentTab" keep-alive></prince>
         </div>
-        <!-- Tab content -->
-        <prince :is="currentTab" keep-alive></prince>
-  </div>
+    </div>
 </template>
 
 <script>
@@ -21,7 +23,10 @@ import ProgramsNewSell from './ProgramsNewSell.vue';
 export default {
     data () {
         return {
-            currentTab: 'ProgramsAll' 
+            currentTab: 'ProgramsAll',
+            isProgramsAllTabfocus: true,
+            isProgramsComingSoonTabfocus: false,
+            isProgramsNewSellTabfocus: false
         };
     },
     components: {
@@ -32,17 +37,35 @@ export default {
     methods: {
         toggleTab: function(tab) {
             this.currentTab = tab;
+        },
+        clickProgramsAll () {
+            this.isProgramsAllTabfocus = true,
+            this.isProgramsComingSoonTabfocus = false,
+            this.isProgramsNewSellTabfocus = false
+        },
+        clickProgramsComingSoon () {
+            this.isProgramsAllTabfocus = false,
+            this.isProgramsComingSoonTabfocus = true,
+            this.isProgramsNewSellTabfocus = false
+        },
+        clickProgramsNewSell () {
+            this.isProgramsAllTabfocus = false,
+            this.isProgramsComingSoonTabfocus = false,
+            this.isProgramsNewSellTabfocus = true
         }
     }
 }
 </script>
 
 <style scoped>
+.programs-bg{
+    background-color: rgb(245, 245, 245);
+    padding-bottom: 100px;
+}
 .programs{
-    border: 1px solid green;
+    /* border: 1px solid green; */
     position: relative;
     width: 1200px;
-    height: 500px;
     margin: 0 auto;
     padding-top: 50px;
   }
@@ -53,10 +76,10 @@ export default {
     border-top: 1px dashed gray;
 }
 .tabs{
-    /* border: 1px solid red; */
+    margin-left: 15px;
+    margin-top: 20px;
 }
 .tab{
-    border: 1px solid;
     border: 2px solid lightgray;
     display: inline-block;
     width: 120px;
@@ -64,12 +87,22 @@ export default {
     text-align: center;
     font-size: 18px;
     transition-duration: .3s;
+    background-color: white;
 }
 .tab:hover{
     cursor: pointer;
     background-color: lightgray;
 }
 .active{
+    background-color: lightgray;
+}
+.programsAllTabfocus{
+    background-color: lightgray;
+}
+.programsComingSoonTabfocus{
+    background-color: lightgray;
+}
+.programsNewSellTabfocus{
     background-color: lightgray;
 }
 </style>
